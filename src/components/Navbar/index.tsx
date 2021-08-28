1d9323412e0e7ce2f7ae3eabe40a4beb55c1e1a4
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-08-28 20:15:02 -0300

    NavBar component created

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { AppBar, Button, makeStyles, Toolbar, Typography, Theme, IconButton, Icon} from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+import logo from '../../static/img/logo.png'
+
+type Props = {
+    
+};
+
+const useStyles = makeStyles((theme : Theme) => ({
+    toolbar: {
+        background: '#3F3F41',
+    },
+    title:{
+        flexGrow: 1,
+        textAlign:'left',
+    },
+    logo:{
+        paddingTop:4,
+        width: 100,
+    },
+    button:{
+        fontSize:9,
+    },
+}));
+
+export const Navbar = (props: Props) => {
+    const classes = useStyles();
+
+    return (
+        <AppBar>
+            
+            <Toolbar variant='dense' className={classes.toolbar}>
+                <Typography className={classes.title}>
+                    <img src={logo} alt="Carlos Transportes" className={classes.logo}/>
+                </Typography>
+                
+                <Button color="inherit" className={classes.button}>Trabalhos realizados</Button>
+                <Button color="inherit" className={classes.button}>Empresas</Button>
+                <Button color="inherit" className={classes.button}>Serviços </Button>
+                <Button color="inherit" className={classes.button}>Contato</Button>
+                <Button color="inherit" className={classes.button}>Sobre nós</Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
