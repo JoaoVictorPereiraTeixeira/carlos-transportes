1f0c7333bbbfcd2c9d65f3111cfe3e9f74b708e6
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2021-09-26 00:11:15 -0300

    fix Indentation

--- src/components/WhoIAm/index.tsx
+++ src/components/WhoIAm/index.tsx
@@ -50,13 +50,13 @@ const WhoIAm = (props: Props) => {
     return (
         <div className={classes.container}>
             <div className={classes.subContainer} >
-            <img src={whoIAm} alt="Quem é Carlos Transportes?" className={classes.whoIAm} />
+              <img src={whoIAm} alt="Quem é Carlos Transportes?" className={classes.whoIAm} />
               
               <p className={classes.presentation}>Olá, sou Alexandre</p>
               <p className={classes.describe}>
                 Realizo entregas a 8 anos is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ips
               </p>
-              </div>
+            </div>
         </div>
 
     );
